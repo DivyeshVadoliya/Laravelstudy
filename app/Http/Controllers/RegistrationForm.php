5a67fe598a6d88c8732a6801b3e3e2349b71abe7
DivyeshVadoliya <d.a.vadoliya@gmail.com>
2022-05-26 21:12:16 +0530

    validated

--- app/Http/Controllers/RegistrationForm.php
+++ app/Http/Controllers/RegistrationForm.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Rules\Uppercase;
+use App\Http\Requests\ErrorRequest;
 class RegistrationForm extends Controller
 {
     //
@@ -11,7 +12,8 @@ class RegistrationForm extends Controller
     {
     	return view('RegistrationForm.formlogin');
     }
-    function signup(Request $request)
+   // function signup(Request $request)
+    function signup(ErrorRequest $request)
     {
     	//dd($request);
     	//print_r($request->input('name')); //printonly name key valyu 
@@ -22,7 +24,26 @@ class RegistrationForm extends Controller
     	print_r($request->old('name'));
     	print_r($request->old('email'));
     	print_r($request->old('password'));*/
-    	$request->flashExcept('password');//without password all data passing
-    	return view('RegistrationForm.formlogin');
+    	//$request->flashExcept('password');//without password all data passing
+
+///////////////////////////////////////////////////////////
+//////////////// validated//////////////////////////////////
+   /* 	$validated=$request->validate([
+    		//'name'=>['required',new Uppercase],//Usercase is make a rules
+    		'name'=>['required',new Uppercase,
+    					function($attribute ,$value , $fail){ //this is special value check it pass it fail massage
+    						if ($value === 'DIVYESH') { 
+    							$fail('This '.$attribute.' is invalid');
+    						}
+    					}
+    				],
+    		'email'=>'required|min:15',
+    		'password'=>'required',
+    	]);
+    	//dd($validated);  */
+//////////////////////////////////////////////////////////
+    	$request->validated();
+    	$input=$request->except('_token');
+    	return view('RegistrationForm.formlogin',['data'=>$input]);
     }
   }
--- resources/views/RegistrationForm/formlogin.blade.php
+++ resources/views/RegistrationForm/formlogin.blade.php
@@ -23,13 +23,35 @@
 		<form action="" method="post">
 			@csrf
 			Name: <input type="taxt" name="name" id="name" value="{{old('name')}}">
+			@error('name')
+				<span>{{$message}}</span>
+			@enderror
 			<br><br>
 			Email: <input type="email" name="email" id="email" value="{{old('email')}}">
+			@error('email')
+				<span>{{$message}}</span>
+			@enderror
 			<br><br>
 			Password: <input type="password" name="password" id="password" value="{{old('password')}}">
+			@error('password')
+				<span>{{$message}}</span>
+			@enderror
 			<br><br>
 			<input type="submit" value="Submit">
 		</form>
+
+
+		{{--@if($errors->any())
+		<h1>{{$errors}}</h1>
+		@endif--}}
+
+
+		@if(isset($data))
+			@foreach($data as $item)
+				<h1>{{$item}}</h1>
+			@endforeach
+		@endif
+
 	</center>
 
 </body>
